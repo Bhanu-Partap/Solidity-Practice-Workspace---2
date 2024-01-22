dfb048e7dac081a7f396d27f785bfae101e001f4
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-01-22 12:07:17 +0530

    dex

--- Custom-Dex/dex.sol
+++ Custom-Dex/dex.sol
@@ -6,6 +6,7 @@ import  "./erc-20.sol";
 contract Pair{
 
     mapping(bytes=> Pool) pools;
+    uint256 INITIAL_LP_TOKENS=1000*10**18;
 
     struct Pool{
         mapping(address=>uint) tokenBalances;
@@ -19,14 +20,14 @@ contract Pair{
         _;
     }
 
-    modifier hasBalanceAndAllowance(address _token0Address, address _token1Address, uint256 _amount0, uint256 _amount1){
+    modifier hasBalanceAndAllowance(address _token0Address, address _token1Address, uint256 _token0Amount, uint256 _token1Amount){
         erc20 token0Address = erc20(_token0Address);
         erc20 token1Address = erc20(_token1Address);  
 
-        require(token0Address.balanceOf(msg.sender)>= _amount0,"Insufficient Balance");
-        require(token1Address.balanceOf(msg.sender)>= _amount1,"Insufficient Balance");
-        require(token0Address.allowance(msg.sender,address(this))== _amount0,"Insufficient allowance for token0");
-        require(token1Address.allowance(msg.sender,address(this))==_amount1,"Insufficient allowance for token1");
+        require(token0Address.balanceOf(msg.sender)>= _token0Amount,"Insufficient Balance");
+        require(token1Address.balanceOf(msg.sender)>= _token1Amount,"Insufficient Balance");
+        require(token0Address.allowance(msg.sender,address(this))== _token0Amount,"Insufficient allowance for token0");
+        require(token1Address.allowance(msg.sender,address(this))==_token1Amount,"Insufficient allowance for token1");
 
         _;
     }
@@ -51,12 +52,14 @@ contract Pair{
 
     }
 
-    function createPair(address _token0Address, address _token1Address, uint256 _token0Amount, uint256 _token1Amount ) validTokenAddresses(_token0Address,  _token1Address)  public returns(uint256){
+    function createPair(address _token0Address, address _token1Address, uint256 _token0Amount, uint256 _token1Amount ) validTokenAddresses(_token0Address,  _token1Address) hasBalanceAndAllowance( _token0Address,  _token1Address,  _token0Amount,  _token1Amount)  public {
         Pool storage pool = getPool(_token0Address, _token1Address);
         require(pool.tokenBalances[_token0Address] ==0,"Pool Already Exist");
         pool.tokenBalances[_token0Address]=_token0Amount;
         pool.tokenBalances[_token1Address]=_token1Amount;
-        
+        pool.lpBalances[msg.sender]= INITIAL_LP_TOKENS;
+        pool.totalLpTokens= INITIAL_LP_TOKENS;
+
 
     }       
 
