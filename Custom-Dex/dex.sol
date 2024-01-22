c564e918ab2b81c1fb9ac74737f0848c1fa1cceb
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-01-22 14:07:11 +0530

    dex

--- Custom-Dex/dex.sol
+++ Custom-Dex/dex.sol
@@ -115,7 +115,7 @@ contract Pair is ReentrancyGuard {
                 pool.tokenBalances[_token1Address] > 0,
             " Token value must be non - zero"
         );
-        return(pool.tokenBalances[_token0Address]* 10**18/ pool.tokenBalances[_token1Address])
+        return(pool.tokenBalances[_token0Address]* 10**18/ pool.tokenBalances[_token1Address]);
     }
 
     function createPool(
@@ -164,10 +164,7 @@ contract Pair is ReentrancyGuard {
     {
         Pool storage pool = getPool(_token0Address, _token1Address);
         uint256 token0Price = getSpotPrice(_token0Address, _token1Address);
-        require(
-            token0Price * _token0Amount * _token1Amount * 10**18,
-            " must add liquidity at current spot price"
-        );
+        require(token0Price * _token0Amount == _token1Amount * 10**18," must add liquidity at current spot price");
         transferToken(_token0Address, _token1Address, _token0Amount,_token1Amount);
         uint currentBalance = pool.tokenBalances[_token0Address];
         uint newTokens = (_token0Amount * INITIAL_LP_TOKENS) / currentBalance;
@@ -179,10 +176,31 @@ contract Pair is ReentrancyGuard {
 
     }
 
-    function removeLiquidity(address _token0Address, address token1Address)
+    function removeLiquidity(address _token0Address, address _token1Address)
+     validTokenAddresses(_token0Address, _token1Address)
+        poolMustExist(_token0Address, _token1Address)
+        nonReentrant
         public
         returns (uint256)
-    {}
+    {
+        Pool storage pool = getPool(_token0Address, _token1Address);
+        uint balance = pool.lpBalances[msg.sender];
+        require(balance > 0,"No liquidity was provided by the user");
+        uint _token0Amount = (balance * pool.tokenBalances[_token0Address]) / pool.totalLpTokens;
+        uint _token1Amount = (balance * pool.tokenBalances[_token1Address]) / pool.totalLpTokens;
+        pool.lpBalances[msg.sender] = 0;
+        pool.tokenBalances[_token0Address] -= _token0Amount;
+        pool.tokenBalances[_token1Address] -= _token1Amount;
+        pool.totalLpTokens -= balance;
+
+        erc20 token0Address = erc20(_token0Address);
+        erc20 token1Address = erc20(_token1Address);
+
+        require(token0Address.transfer(msg.sender, _token0Amount), "Transfer  token0 failed !!");
+        require(token1Address.transfer(msg.sender, _token1Amount), "Transfer  token1 failed !!");
+
+
+    }
 
     function swap(
         address from,
