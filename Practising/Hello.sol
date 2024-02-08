2d48c200d5e35a11a5dd609cfbd5b70d1b0ffdca
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-02-08 17:34:13 +0530

    fixed array

---
+++ Practising/Hello.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract FixedSizeArray {
+    // Declare a fixed size array of 5 integers
+    uint[5] public fixedArray;
+    
+
+    // Constructor
+    constructor() {
+        // Initialize the array with some values
+        fixedArray[0] = 1;
+        fixedArray[1] = 2;
+        fixedArray[2] = 3;
+        fixedArray[3] = 4;
+        fixedArray[4] = 5;
+    }
+
+    // Function to push a value to the array
+    function push() public {
+        // This will fail because the array is already full
+        for(uint256 i = 0; i<=fixedArray.length;i++){
+        fixedArray.push(i) ;
+
+        }
+    }
+}
